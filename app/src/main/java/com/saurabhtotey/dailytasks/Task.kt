a9617ada4df57278161f110b9bbcb72ab5f50c6d
SaurabhTotey <SaurabhTotey@gmail.com>
2019-12-21 21:59:03 -0700

    Made an encoding structure for task data

---
+++ app/src/main/java/com/saurabhtotey/dailytasks/Task.kt
@@ -0,0 +1,104 @@
+package com.saurabhtotey.dailytasks
+
+/**
+ * An enumeration of the different types of ways a task can be filled out
+ * CHECKBOX should be used for boolean values
+ * POSITIVE_INTEGER should be used for tasks that take in a positive integer that allows 0
+ * NONE should be used for tasks that their completion off of some other criteria (eg. sub-tasks)
+ */
+enum class FormType {
+	CHECKBOX, POSITIVE_INTEGER, NONE
+}
+
+/**
+ * An enumeration of all the daily tasks I must complete
+ *
+ * displayName is how the task will be displayed to the user
+ * description is a more in-depth explanation of what the task entails or requires
+ * formType takes in a FormType enumeration to specify how the user will input data for the task
+ * formDescription describes the units or inputs for the form
+ * evaluateIsCompleted is a function that takes in form data (more on form data below) and evaluates whether the task counts as completed or not (null return means completion is irrelevant: it isn't completed or not completed)
+ * subTasks is an array of tasks that will visually appear to be a subordinate task to the current task or will visually appear to be hierarchically below or included as a part of the current task
+ *
+ * The actual caps name for the enumeration values is what is recorded when task data is written to file, along with integers describing their data
+ * A series of integers is recorded as a part of task data: the first integer corresponds to the data of the current task while subsequent integers represent the data for the subTasks in the same order as the subTasks are listed in the task data
+ * These integers are what gets passed to evaluateIsCompleted
+ *
+ * A checkbox will record values as either 0s or 1s for the corresponding boolean values
+ * A positive integer will record values as its integer
+ * An empty form type (NONE) will record as always 0s
+ *
+ * TODO: consider storing task data as doubles or something
+ * TODO: consider having a class that represents task data that pairs the numerical value with a day or date or time or something so that the data object can be passed in a list to evaluateIsCompleted so evaluation can also consider completion date
+ */
+enum class Task(val displayName: String, val description: String, val formType: FormType = FormType.CHECKBOX, val formDescription: String = "", val evaluateIsCompleted: (Array<Int>) -> Boolean? = { it[0] > 0 }, val subTasks: Array<Task> = arrayOf()) {
+	MEDITATE(
+		"Meditate",
+		"Meditate for at least 5 minutes.",
+		FormType.POSITIVE_INTEGER,
+		"minutes",
+		{ it[0] >= 5 }
+	),
+	SHOWER(
+		"Shower",
+		"Take a shower."
+	),
+	BRUSH_TEETH(
+		"Brush Teeth",
+		"Brush your teeth at least twice today.",
+		FormType.POSITIVE_INTEGER,
+		"times"
+	),
+	PRACTICE(
+		"Practice Musical Instrument(s)",
+		"Spend at least 30 minutes practicing the piano. If possible, also practice cello.",
+		FormType.POSITIVE_INTEGER,
+		"minutes"
+	),
+	EAT_HEALTHY("Eat Healthy Meals", "Eat healthy meals.", FormType.POSITIVE_INTEGER, "meals"),
+	EAT_MISCELLANEOUS("Eat Meals", "Eat meals.", FormType.POSITIVE_INTEGER, "meals", { null }),
+	EAT(
+		"Eat",
+		"Eat at least two meals today. At least one of those meals needs to be healthy.",
+		FormType.NONE,
+		"",
+		{ it[1] > 0 && it[1] + it[2] >= 2 },
+		arrayOf(EAT_HEALTHY, EAT_MISCELLANEOUS)
+	),
+	EXERCISE(
+		"Exercise",
+		"Exercise for at least 20 minutes.",
+		FormType.POSITIVE_INTEGER,
+		"minutes",
+		{ it[0] >= 20 }
+	),
+	COMMIT(
+		"Make a Code Commit",
+		"Commit a change on a git repository."
+	),
+	LEARN_PHYSICS(
+		"Learn New Physics",
+		"Learn some new physics that you didn't know before."
+	),
+	SOLVE_PROBLEM(
+		"Solve a Problem",
+		"Solve any sort of problem. It could be homework, a personal problem, or anything else."
+	),
+	TALK_NEW("Meet a New Person", "Meet and talk with someone you don't know.", FormType.CHECKBOX, "", { if (it[0] > 0) true else null }),
+	TALK_QUESTION("Ask a Question", "Ask anyone any sort of question. It could be clarification, or asking for help, or anything else.", FormType.CHECKBOX, "", { if (it[0] > 0) true else null }),
+	TALK(
+		"Talk",
+		"Either meet a new person or ask someone familiar a legitimate question.",
+		FormType.NONE,
+		"",
+		{ it[1] > 0 || it[2] > 0 },
+		arrayOf(TALK_NEW, TALK_QUESTION)
+	),
+	PLANK(
+		"Plank",
+		"Plank for at least 4 minutes (240 seconds).",
+		FormType.POSITIVE_INTEGER,
+		"seconds",
+		{ it[0] >= 240 }
+	)
+}
