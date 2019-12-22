db81f6bf5bbc8c99761410ff2d76e70720ae9e3b
SaurabhTotey <SaurabhTotey@gmail.com>
2019-12-22 16:49:22 -0700

    Refactored TaskDataController structure and completed functionality

--- app/src/main/java/com/saurabhtotey/dailytasks/TaskDataController.kt
+++ app/src/main/java/com/saurabhtotey/dailytasks/TaskDataController.kt
@@ -15,31 +15,37 @@ object TaskDataController {
 
 	private val file = File("SaurabhToteyTaskData.json")
 	private var fileData: JSONArray
-	private var currentDayTasksData: JSONObject
+	private var currentDayTasksData: JSONObject = JSONObject()
 
 	/**
-	 * Reads from the file to check if data from this day exists already
-	 * If no data on file for today, create an entry
-	 * Guarantees that today's entry is the first entry of fileData
-	 * TODO: this code won't work well if the app is left open between days: it will need to be re-run
+	 * Parses the data file
+	 * Creates data file if it doesn't exist
 	 */
 	init {
-		if (!file.exists()) {
-			file.createNewFile()
-			file.writeText("[]")
+		if (!this.file.exists()) {
+			this.file.createNewFile()
+			this.file.writeText("[]")
 		}
+		this.fileData = JSONArray(this.file.readLines().joinToString("\n"))
+		this.initializeNewDayData()
+	}
 
-		fileData = JSONArray(file.readLines().joinToString("\n"))
+	/**
+	 * Gets task data for today from fileData
+	 * If no data on file for today, create an entry
+	 * Guarantees that today's entry is the first entry of fileData
+	 * TODO: must be called again if the app is open but the date changes
+	 */
+	private fun initializeNewDayData() {
 		val currentDate = this.getDateString()
-
-		if (fileData.length() == 0 || (fileData[0] as JSONObject).getString("date") != currentDate) {
+		if (this.fileData.length() == 0 || (this.fileData[0] as JSONObject).getString("date") != currentDate) {
 			this.currentDayTasksData = JSONObject()
 			this.currentDayTasksData.put("date", currentDate)
-			this.currentDayTasksData.put("data", JSONObject()) //TODO: this should be a map from Task enum name to list of integer values
-			(fileData.length() downTo 1).forEach { i -> fileData.put(i, fileData.get(i - 1)) }
+			this.currentDayTasksData.put("data", JSONObject())
+			(this.fileData.length() downTo 1).forEach { i -> this.fileData.put(i, this.fileData.get(i - 1)) }
 			this.handleUpdate()
 		} else {
-			this.currentDayTasksData = fileData.get(0) as JSONObject
+			this.currentDayTasksData = this.fileData.get(0) as JSONObject
 		}
 	}
 
@@ -52,6 +58,8 @@ object TaskDataController {
 
 	/**
 	 * Updates all JSON objects and ensures that the file is up-to-date
+	 * Should be called whenever currentDayTasksData is updated or fileData is updated
+	 * Changes from currentDayTasksData take highest precedence (eg. will overwrite changes made in fileData if they differ)
 	 */
 	private fun handleUpdate() {
 		this.fileData.put(0, this.currentDayTasksData)
@@ -62,14 +70,38 @@ object TaskDataController {
 	 * Gets the stored value for the given task: defaults to 0
 	 */
 	fun getValueFor(task: Task): Int {
-		return 0 //TODO:
+		if ((this.currentDayTasksData["data"] as JSONObject).keys().asSequence().contains(task.name)) {
+			return (this.currentDayTasksData["data"] as JSONObject).getInt(task.name)
+		}
+		return 0
+	}
+
+	/**
+	 * Given a target task, creates an array of relevant values for the target task
+	 * First element of the array is the value of the task itself, and the subsequent values are the values for the sub-tasks in the order they are listed as sub-tasks
+	 */
+	fun getValuesForTask(task: Task): Array<Int> {
+		return Array(task.subTasks.size + 1) { i -> this.getValueFor(if (i == 0) task else task.subTasks[i - 1]) }
 	}
 
 	/**
 	 * Updates the given task to have the given value
+	 * If a value is set to 0, the task is just removed from the data as an omission defaults to 0
 	 */
 	fun setValueForTask(task: Task, value: Int) {
-		//TODO:
+		if (value == 0) {
+			(this.currentDayTasksData["data"] as JSONObject).remove(task.name)
+		} else {
+			(this.currentDayTasksData["data"] as JSONObject).put(task.name, value)
+		}
+	}
+
+	/**
+	 * Filters all the tasks and gets all non-sub-tasks
+	 */
+	fun getPrimaryTasks(): Array<Task> {
+		val subTasks = Task.values().flatMap { it.subTasks.toList() }
+		return Task.values().filter { !subTasks.contains(it) }.toTypedArray()
 	}
 
 }
